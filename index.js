426b7b964e937e48b219f970751de0b31dac4f13
Timih124 <tim.hackel24@gmail.com>
2022-04-26 19:19:36 -0500

    adding save button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//testing to display 5 on html page
 //document.getElementById("count-sub").innerText = 5
 
 
@@ -11,6 +13,13 @@ let count = 0
 function increase() {
     count = count + 1
     countSub.innerText = count
-    console.log("The button was clicked")
+   //console.log("The button was clicked")
+    //console.log(count)
+
+}
+
+// creating save function to log and save count
+function save(){
+
     console.log(count)
 }
