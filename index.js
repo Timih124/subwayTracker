b111fdc82286fed2fc1ab3a88e15733135f45309
Timih124 <tim.hackel24@gmail.com>
2022-04-26 19:10:44 -0500

    adding interactive button counter using javascript

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+//document.getElementById("count-sub").innerText = 5
 
 
+let countSub = document.getElementById("count-sub")
+
+console.log("count-sub")
+
+let count = 0 
+
+// increase count everytime button is clicked
 function increase() {
+    count = count + 1
+    countSub.innerText = count
     console.log("The button was clicked")
+    console.log(count)
 }
